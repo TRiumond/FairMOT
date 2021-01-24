51c8e2844db99267fa90aa11c29fdc43197e4397
hulkbulk <Espace23>
2021-01-24 15:40:56 +0100

    [FiX] put latest know working commit

--- src/_init_paths.py
+++ src/_init_paths.py
@@ -5,7 +5,7 @@ try:
     import dcn_v2
 except ImportError:
     import pip
-    pip.main(['install', '-e','git+https://github.com/CharlesShang/DCNv2#egg=DCNv2', '--user'])
+    pip.main(['install', '-e','git+https://github.com/CharlesShang/DCNv2@c7f778f28b84c66d3af2bf16f19148a07051dac1#egg=DCNv2', '--user'])
     sys.path.insert(0, "./src/dcnv2")
     import dcn_v2
 
